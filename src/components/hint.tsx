cf6889a85870fe840f4b4def36fc11f1ad0a5cc7
JJeCho <james.je.cho@gmail.com>
2024-08-27 21:32:53 -0700

    workspace sidebar

---
+++ src/components/hint.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+
+interface HintProps {
+    label: string;
+    children: React.ReactNode;
+    side?: "bottom" | "left" | "right" | "top";
+    align?: "start" | "center" | "end";
+}
+
+export const Hint = ({
+    label,
+    children,
+    side,
+    align
+}: HintProps) => {
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent side={side} align={align} className="bg-black text-white border border-white/5">
+                    <p className="font-medium text-xs">{label}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+
+}
