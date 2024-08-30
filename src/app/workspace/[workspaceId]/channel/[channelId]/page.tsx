6e26d591d97391aacaea20dd94ce8ea7403571c2
James Cho <james.je.cho@gmail.com>
2024-08-30 09:31:31 -0700

    messages get api

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,13 +5,15 @@ import { useChannelId } from "@/hooks/use-channel-id";
 import { Loader, TriangleAlert } from "lucide-react";
 import { ChatInput } from "./chat-input";
 import { Header } from "./header";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
+  const {results} = useGetMessages({channelId})
   const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId})
 
-  
+  console.log(results)
   if (channelLoading) {
     return <div className="h-full flex-1 flex items-center justify-center">
       <Loader className="animate-spin size-5 text-muted-foreground"/>
@@ -28,7 +30,9 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
-      <div className="flex-1" />
+      <div className="flex-1">
+        {JSON.stringify(results)}
+      </div>
       <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   )
