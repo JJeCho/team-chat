c9c7b166dcd386dabe4848705258a10b990827bb
JJeCho <james.je.cho@gmail.com>
2024-09-01 23:06:34 -0700

    complete role functionality

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,10 +1,18 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
-import { AlertTriangle, Loader, MailIcon, XIcon } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { AlertTriangle, ChevronDownIcon, Loader, MailIcon, XIcon } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { Id } from "../../../../convex/_generated/dataModel";
+import { useCurrentMember } from "../api/use-current-members";
 import { useGetMember } from "../api/use-get-member";
+import { useRemoveMember } from "../api/use-remove-member";
+import { useUpdateMember } from "../api/use-update-member";
 
 interface ProfileProps {
     memberId: Id<"members">;
@@ -12,9 +20,72 @@ interface ProfileProps {
 }
 
 export const Profile = ({memberId, onClose}: ProfileProps) => {
+    const router = useRouter();
+    const workspaceId = useWorkspaceId();
+    const [LeaveDialog, confirmLeave] = useConfirm(
+        "Leave Workspace",
+        "Are you sure you want to leave this workspace?"
+    );
+
+    const [RemoveDialog, confirmRemove] = useConfirm(
+        "Remove Member",
+        "Are you sure you want to remove this member?"
+    );
+
+    const [UpdateDialog, confirmUpdate] = useConfirm(
+        "Change Role",
+        "Are you sure you want to change this user's role?"
+    );
+    const {data: currentMember, isLoading: isLoadingCurrentMember} = useCurrentMember({workspaceId});
+    const {mutate: updateMember, isPending: isUpdatingMember} = useUpdateMember();
+    const {mutate: removeMember, isPending: isRemovingMember} = useRemoveMember();
     const {data: member, isLoading: isLoadingMember} = useGetMember({id: memberId});
     const avatarFallback = member?.user.name?.[0] ?? "M";
-    if (isLoadingMember) {
+    
+    const onRemove = async () => {
+        const ok = await confirmRemove();
+        if (!ok) return;
+        removeMember({id: memberId}, {
+            onSuccess: () => {
+                toast.success("Member removed");
+                onClose();
+            },
+            onError: () => {
+                toast.error("Failed to remove member");
+            }
+        });
+    }
+
+    const onLeave = async () => {
+        const ok = await confirmLeave();
+        if (!ok) return;
+        removeMember({id: memberId}, {
+            onSuccess: () => {
+                router.replace("/");
+                toast.success("You left the workspace");
+                onClose();
+            },
+            onError: () => {
+                toast.error("Failed to leave workspace");
+            }
+        });
+    }
+
+    const onUpdate = async (role: "admin" | "member") => {
+        const ok = await confirmUpdate();
+        if (!ok) return;
+        updateMember({id: memberId, role}, {
+            onSuccess: () => {
+                toast.success("Role Changed");
+                onClose();
+            },
+            onError: () => {
+                toast.error("Failed to change role");
+            }
+        });
+    }
+
+    if (isLoadingMember || isLoadingCurrentMember) {
         return (
           <div className="h-full flex flex-col">
             <div className="h-[49px] flex justify-between items-center px-4 border-b">
@@ -48,6 +119,10 @@ export const Profile = ({memberId, onClose}: ProfileProps) => {
       }
     
     return (
+        <>
+        <RemoveDialog />
+        <LeaveDialog />
+        <UpdateDialog />
         <div className="h-full flex flex-col">
             <div className="h-[49px] flex justify-between items-center px-4 border-b">
               <p className="text-lg font-bold">Profile</p>
@@ -65,6 +140,43 @@ export const Profile = ({memberId, onClose}: ProfileProps) => {
             </div>
             <div className="flex flex-col p-4">
                 <p className="text-lg font-bold">{member.user.name}</p>
+                {
+                    currentMember?.role === "admin" &&
+                    currentMember?._id !== memberId ? (
+                        <div className="flex items-center gap-2 mt-4">
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="w-full capitalize" >
+                            {member.role} <ChevronDownIcon className="size-4 ml-2" />
+                        </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-full">
+                            <DropdownMenuRadioGroup
+                            value={member.role}
+                            onValueChange={(role) => onUpdate(role as "admin" | "member")}
+                            >
+                                <DropdownMenuRadioItem value="admin">
+                                    Admin
+                                </DropdownMenuRadioItem>
+                                <DropdownMenuRadioItem value="member">
+                                    Member
+                                </DropdownMenuRadioItem>
+                            </DropdownMenuRadioGroup>
+                        </DropdownMenuContent>
+                        </DropdownMenu>
+                        <Button onClick={onRemove} variant="outline" className="w-full">
+                            Remove
+                        </Button>
+                        </div>
+                    ) : currentMember?._id === memberId &&
+                    currentMember?.role !== "admin" ? (
+                        <div className="mt-4">
+                            <Button onClick={onLeave} variant="outline" className="w-full">
+                            Leave
+                        </Button>
+                        </div>
+                    ) : null
+                }
             </div>
             <Separator />
             <div className="flex flex-col p-4">
@@ -86,5 +198,6 @@ export const Profile = ({memberId, onClose}: ProfileProps) => {
                 </div>
             </div>
           </div>
+        </>
     )
 }
